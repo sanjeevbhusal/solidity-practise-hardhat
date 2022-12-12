475c55dc9185a6010d73d2f41e1b3a191921fb46
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-12-12 18:25:48 +0545

    cleared the extra files and updated package.json to include scripts

--- tasks/blockNumber.js
+++ tasks/blockNumber.js
@@ -1,6 +1,6 @@
-const { task } = require("hardhat/config");
+const { task } = require('hardhat/config');
 
-task("block_number", "prints the current block number").setAction(
+task('block_number', 'prints the current block number').setAction(
   async (taskArgs, hre) => {
     let blockNumber = await hre.ethers.provider.getBlockNumber();
     console.log(blockNumber);
